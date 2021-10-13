2c05fb36ea8e26ac6082cc4d52e0879c34c142ca
nicolas-rodriguez-2 <a2018121014@estudiantes.upsa.edu.bo>
2021-10-13 10:55:07 -0400

    Crear clase Models

---
+++ CancionAPI/Models/Cancion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CancionAPI.Models
+{
+    public class Cancion
+    {
+
+        [Key]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Ingrese nombre de la cancion")]
+        [Display(Name = "Nombre de la Cancion")]
+        public string NombreCancion { get; set; }
+
+
+
+        [Required] //primero las condiciones
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Ingrese autor/compositor de la cancion")]
+        [Display(Name = "Autor/Compositor")]
+        public string Autor { get; set; } //luego se muestra que chota
+
+
+
+        [Required]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Ingrese Letra de la cancion")]
+        [Display(Name = "Letra")]
+        public string Letra { get; set; }
+
+
+
+        [Url] //solo cuando es un link xd, caso contrario required
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Ingrese Enlace de la cancion")]
+        [Display(Name = "Enlace")]
+        public string Enlace { get; set; }
+    }
+}
